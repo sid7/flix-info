a0c92fd59f6af06e104588fc7859f87ea0b37c3e
Suyash Bhutani <sid7@users.noreply.github.com>
2021-10-13 01:49:28 +0530

    chore: upgrade package dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
-
+import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    jsxInject: 'import React from "react";'
-  },
   server: {
     open: true,
-    port: 8006
+    port: 8006,
   },
-  plugins: [reactRefresh()]
+  plugins: [react()],
 });
